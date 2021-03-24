036fe872d4438e03c481197d895ede1406fdd896
yura0seredyuk <yura.seredyuk0@gmail.com>
2021-03-24 19:44:49 +0200

    Updates

---
+++ precache-manifest.924035832513d839de2eba0f693a835a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e15ac3fda57cda08c2224b220638d52a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3d5b128c039408963e90",
+    "url": "./static/css/main.31b0181a.chunk.css"
+  },
+  {
+    "revision": "d11e77adf2a447d9d893",
+    "url": "./static/js/2.924ae9d3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.924ae9d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d5b128c039408963e90",
+    "url": "./static/js/main.012c14a3.chunk.js"
+  },
+  {
+    "revision": "de889ebc4391832068f6",
+    "url": "./static/js/runtime-main.311b6371.js"
+  }
+]);
